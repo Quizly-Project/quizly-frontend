c25cb2d7e14b2cfc9f694a18c21d7451af520367
hyunS00 <optifun72@gmail.com>
2024-07-01 01:37:54 +0900

    방코드 입력 page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import {
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import CreateQuiz from './pages/CreateQuiz';
+import Landing from './pages/Landing.jsx';
 import Layout from './components/layout/Layout.jsx';
 import useAuthStore from './store/authStore';
 
@@ -32,6 +33,7 @@ function App() {
         <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/create/:quizType" element={<CreateQuiz />} />
+        <Route path="/landing" element={<Landing />} />
       </Routes>
     </Router>
   );
