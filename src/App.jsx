ba4aa1336c6583451fcbe717d679a007ea0c3161
hyunS00 <optifun72@gmail.com>
2024-06-30 15:34:49 +0900

    Signup Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useForm } from 'react-hook-form';
 import './styles/App.css';
+import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 
 function App() {
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,7 +4,7 @@ import Button from '../components/common/Button';
 import InputField from '../components/common/InputField';
 import Text from '../components/common/Text';
 
-const SignIn = () => {
+const SignUp = () => {
   const {
     register,
     handleSubmit,
@@ -18,14 +18,14 @@ const SignIn = () => {
   return (
     <>
       <Text align="center" type="title">
-        로그인
+        회원가입
       </Text>
       <form onSubmit={handleSubmit(onSubmit)}>
         <InputField
           name="id"
           type="email"
           className="center"
-          placeholder="이메일을 입력하세요."
+          placeholder="이메일"
           register={register}
           errors={errors}
           required="Email is required"
@@ -40,7 +40,21 @@ const SignIn = () => {
           type="password"
           className="center"
           register={register}
-          placeholder="비밀번호를 입력하세요."
+          placeholder="비밀번호"
+          errors={errors}
+          required="Password is required"
+          round={true}
+          minLength={{
+            value: 8,
+            message: 'Password must be at least 8 characters',
+          }}
+        />
+        <InputField
+          name="check_pw"
+          type="password"
+          className="center"
+          register={register}
+          placeholder="비밀번호 확인"
           errors={errors}
           required="Password is required"
           round={true}
@@ -51,13 +65,13 @@ const SignIn = () => {
         />
 
         <Button wide={true} round={true}>
-          로그인
+          회원가입
         </Button>
       </form>
       <Button wide={true} round={true} color="secondary">
-        회원가입
+        로그인으로 이동
       </Button>
     </>
   );
 };
-export default SignIn;
+export default SignUp;
