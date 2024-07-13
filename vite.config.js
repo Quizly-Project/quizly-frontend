a50446447d35136217c229345ed9b4944e03709c
jjr7181 <jjr7181@gmail.com>
2024-07-13 21:18:01 +0900

    feat: openvidu 화상 채팅 test

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  server: {
-    host: true,
-    port: 3001,
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
-});
+  esbuild: {
+    loader: 'tsx',
+    include: /src\/.*\.[tj]sx?$/,
+    exclude: []
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+        '.ts': 'tsx'
+      }
+    }
+  }
+})
