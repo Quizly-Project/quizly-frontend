993f481eb881f33fb5fa70ca622b02935da22211
hyunS00 <optifun72@gmail.com>
2024-07-17 23:47:20 +0900

    feat: 학생 클릭시 화상카메라 선생님으로 전환

--- src/components/Game/MyCameraOtherVoice/MyCameraOtherVoice.tsx
+++ src/components/Game/MyCameraOtherVoice/MyCameraOtherVoice.tsx
@@ -4,25 +4,31 @@ import VideoComponent from '../LiveKit/components/VideoComponent';
 import AudioComponent from '../LiveKit/components/AudioComponent'; // Update import statement
 import { useLiveKitStore } from '../../../store/liveKitStore';
 import useQuizRoomStore from '../../../store/quizRoomStore';
+import RemoteVideoDisplay from '../RemoteVideoDisplay/RemoteVideoDisplay';
 
 interface LiveKitProps {
   quizResult: any;
+  selectedStudent: string;
 }
 
-const MyCameraOtherVoice: React.FC<LiveKitProps> = () => {
+const MyCameraOtherVoice: React.FC<LiveKitProps> = ({ selectedStudent }) => {
   const { localTrack, remoteTracks } = useLiveKitStore();
   const { nickName } = useQuizRoomStore(state => state.quizRoom);
-
   return (
     <div id="room">
       <div id="layout-container">
-        {localTrack && (
-          <VideoComponent
-            track={localTrack}
-            participantIdentity={nickName}
-            local={true}
-          />
+        {selectedStudent ? (
+          <RemoteVideoDisplay participantId={selectedStudent} />
+        ) : (
+          localTrack && (
+            <VideoComponent
+              track={localTrack}
+              participantIdentity={nickName}
+              local={true}
+            />
+          )
         )}
+
         {remoteTracks.map(remoteTrack => {
           const audioTrack = remoteTrack.trackPublication.audioTrack;
           return audioTrack ? (
