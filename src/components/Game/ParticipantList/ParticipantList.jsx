70af26780baa9e2cf7d40ef35280a871e8ce5a2c
yunnn426 <yunnn99@gmail.com>
2024-07-24 18:09:46 +0900

    style: 참가자 목록 스타일 수정

--- src/components/Game/ParticipantList/ParticipantList.jsx
+++ src/components/Game/ParticipantList/ParticipantList.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect } from 'react';
-import { Track } from 'livekit-client';
-import styles from './ParticipantList.module.css';
+import React from 'react';
 import { useLiveKitStore } from '../../../store/liveKitStore';
+import styles from './ParticipantList.module.css';
 
 const ParticipantList = ({
   participants,
@@ -13,28 +12,35 @@ const ParticipantList = ({
 
   return (
     <div className={styles.participantList}>
-      <h3 className={styles.title}>참가자 목록</h3>
+      {/* <h3 className={styles.title}>참가자 목록</h3> */}
       {isTeacher && selectedStudent && (
         <button
           className={styles.resetButton}
           onClick={() => onSelectStudent(null)}
         >
-          내 시점으로 돌아가기
+          내 시점으로
         </button>
       )}
       <ul className={styles.list}>
-        {participants.map(participant => (
-          <li
-            key={participant.nickName}
-            className={`${styles.participant} ${participant.nickName === selectedStudent ? styles.selected : ''} ${participant.isTeacher ? styles.teacher : ''} ${currentSpeakers.includes(participant.nickName) ? styles.speaking : ''}`}
-            onClick={() => isTeacher && onSelectStudent(participant.nickName)}
-          >
-            <span className={styles.nickname}>{participant.nickName}</span>
-            {participant.isTeacher && (
-              <span className={styles.teacherBadge}>선생님</span>
-            )}
-          </li>
-        ))}
+        {participants.map(participant => {
+          const iconClass = participant ? styles[participant.icon] : '';
+
+          return (
+            <li
+              key={participant.nickName}
+              className={`${styles.participant} ${participant.nickName === selectedStudent ? styles.selected : ''} ${participant.isTeacher ? styles.teacher : ''} ${currentSpeakers.includes(participant.nickName) ? styles.speaking : ''}`}
+              onClick={() => isTeacher && onSelectStudent(participant.nickName)}
+            >
+              <div className={styles.participantInfo}>
+                <div className={`${styles.participantIcon} ${iconClass}`}></div>
+                <span className={styles.nickname}>{participant.nickName}</span>
+              </div>
+              {participant.isTeacher && (
+                <span className={styles.teacherBadge}>선생님</span>
+              )}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
