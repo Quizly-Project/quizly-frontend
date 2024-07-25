117f500be5e6778d5c709c73734ddb1a80526eb4
jjr7181 <jjr7181@gmail.com>
2024-07-26 01:38:57 +0900

    feat: nickname input css 수정

--- src/components/Game/NickNameInput/NickNameInput.jsx
+++ src/components/Game/NickNameInput/NickNameInput.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react';
 import Button from '../../common/Button/Button';
 import styles from './NickNameInput.module.css';
 
-const NickNameInput = ({ onClick, ...props }) => {
+const NickNameInput = ({ onClick }) => {
   const [inputNickName, setInputNickName] = useState('');
 
   // onClick prop이 함수인지 확인하는 방어 코드 추가
-  const handleOnClick = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault(); // 폼 기본 제출 동작 방지
     if (typeof onClick === 'function') {
       onClick(inputNickName);
     } else {
@@ -15,17 +16,19 @@ const NickNameInput = ({ onClick, ...props }) => {
   };
 
   return (
-    <div className={`${styles.inputContainer} alignCenter`}>
+    <form onSubmit={handleSubmit} className={styles.inputContainer}>
       <input
+        className={styles.inputField}
         type="text"
         value={inputNickName}
-        onChange={e => setInputNickName(e.target.value)}
+        onChange={(e) => setInputNickName(e.target.value)}
         placeholder="닉네임을 입력하세요"
-        className={styles.inputField}
-        {...props}
       />
-      <button onClick={handleOnClick}>확인</button>
-    </div>
+
+      <button type="submit" className={styles.gameButton}>
+        게임 시작!
+      </button>
+    </form>
   );
 };
 
